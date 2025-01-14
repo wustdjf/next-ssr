1896882bd114416989c35276043dac21d4a33315
邓俊锋 <dengjf@zenmen.com>
2025-01-14 18:05:15 +0800

    feat: 服务端渲染

--- src/app/[locale]/next/isr/[id]/page.tsx
+++ src/app/[locale]/next/isr/[id]/page.tsx
@@ -42,8 +42,11 @@ export default async function ISRPage({ params }: { params: Promise<{ id: string
   const id = (await params).id
   const post: { title: string; content: string } = await getDataById(id)
   console.log('ISRPage', id, post)
+
   return (
     <main>
+      <a href={'/'}>返回首页</a>
+      <p>[id]定义动态路由</p>
       <h1>{post.title}</h1>
       <p>{post.content}</p>
     </main>
